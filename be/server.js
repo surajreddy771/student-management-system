45a3b3fec2df19facf5169de39b80cae213c426f
surajreddy771 <surajreddy771@gmail.com>
2025-04-22 21:54:53 +0530

    Add log for MongoDB URI

--- be/server.js
+++ be/server.js
@@ -11,6 +11,7 @@ const studentRoutes = require('./routes/students');
 app.use(cors());
 app.use(express.json());
 app.use('/students', studentRoutes);
+console.log('Connecting to:', process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
