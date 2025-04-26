e376dcccd6ea911c82b328de6189510183cc42be
surajreddy771 <152050253+surajreddy771@users.noreply.github.com>
2025-04-26 20:23:36 +0530

    Update App.jsx

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
 import AddStudent from './components/AddStudent';
 import StudentList from './components/StudentList';
@@ -8,11 +9,12 @@ import EditStudent from './components/EditStudent';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
         <Route path="/students" element={<StudentList />} />
         <Route path="/add" element={<AddStudent />} />
         <Route path="/edit/:id" element={<EditStudent />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
